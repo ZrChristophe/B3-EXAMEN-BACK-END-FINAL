87d6b0f84f612dd3745ed311240cead9582de788
ZrChristophe <cuichristophe.1@gmail.com>
2025-12-09 11:31:03 +0100

    modfier

--- src/auth/strategies/jwt-access.strategy.ts
+++ src/auth/strategies/jwt-access.strategy.ts
@@ -8,7 +8,7 @@ export class JwtAccessStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor(configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: configService.get<string>('JWT_ACCESS_SECRET'),
+      secretOrKey: configService.get<string>('JWT_ACCESS_SECRETlplplplp'),
     });
   }
 
